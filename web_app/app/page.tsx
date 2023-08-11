e2462aae1d72db12d5d8b9deb9fd0a8a9e7a2a6a
iandrej <andrej@okedoc.fr>
2023-08-11 11:25:05 +0200

    Added basic CRUD functionality for the hospital service. I need to test things properly, and add front end tests.

--- web_app/app/page.tsx
+++ web_app/app/page.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image'
 import React from "react";
-import HospitalList from "@/components/HospitalList";
 import PatientList from "@/components/PatientList";
 
 export default function Home() {
   return (
       <div>
         <h1>Welcome to MedHead Frontend</h1>
-          <HospitalList />
           <PatientList/>
       </div>
   )
--- web_app/pages/hospitals/index.tsx
+++ web_app/pages/hospitals/index.tsx
@@ -1,42 +1,53 @@
-"use client";
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 interface Hospital {
     id: number;
     name: string;
     location: string;
+    numberOfAllBeds: number;
+    numberOfAvailableBeds: number;
+    numberOfUnavailableBeds: number;
+    specializations: string[];
 }
 
-function HospitalList() {
+const HospitalList = () => {
     const [hospitals, setHospitals] = useState<Hospital[]>([]);
 
     useEffect(() => {
-        async function fetchHospitals() {
-            try {
-                const response = await axios.get<Hospital[]>('http://localhost:9000/api/hospitals');
-                setHospitals(response.data);
-            } catch (error) {
-                console.error("Error fetching hospitals:", error);
-            }
-        }
+        const fetchData = async () => {
+            const response = await fetch('http://localhost:9000/api/hospitals');
+            const data = await response.json();
+            setHospitals(data);
+        };
 
-        fetchHospitals();
+        fetchData();
     }, []);
 
     return (
-        <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
-            <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Hospitals</h2>
-            <ul style={{ listStyleType: 'none', padding: 0 }}>
-                {hospitals.map(hospital => (
-                    <li key={hospital.id} style={{ padding: '10px', borderBottom: '1px solid #e1e1e1' }}>
-                        <h3>{hospital.name}</h3>
-                        <p><strong>Location:</strong> {hospital.location}</p>
+        <div>
+            <h1>Hospitals</h1>
+            <ul>
+                {hospitals.map((hospital) => (
+                    <li key={hospital.id}>
+                        <Link href={`/hospitals/${hospital.id}`}>
+                            {hospital.name} - {hospital.location}
+                        </Link>
+                        <p>Total Beds: {hospital.numberOfAllBeds}</p>
+                        <p>Available Beds: {hospital.numberOfAvailableBeds}</p>
+                        <p>Unavailable Beds: {hospital.numberOfUnavailableBeds}</p>
+                        <p>Specializations:</p>
+                        <ul>
+                            {hospital.specializations.map((specialization, index) => (
+                                <li key={index}>{specialization}</li>
+                            ))}
+                        </ul>
+                        <br />
                     </li>
                 ))}
             </ul>
         </div>
     );
-}
+};
 
 export default HospitalList;
